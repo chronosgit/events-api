b7c35d06475268edc1d7900b3312db4385d8b007
chronosgit <warfacemeister@mail.ru>
2024-05-08 22:47:06 +0500

    feat: put all schemas into models.js

--- models.js
+++ models.js
@@ -1,8 +1,52 @@
 const mongoose = require('mongoose');
-const eventSchema = require('./schemas.js');
-const userSchema = require('./schemas.js');
 
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	refreshToken: String,
+	tickets: [
+		{type: mongoose.Schema.Types.ObjectId, ref: "Event"},
+	],
+});
 const User = mongoose.model("User", userSchema);
+
+const eventSchema = new mongoose.Schema({
+	image: {
+		data: Buffer,
+		contentType: String,
+	},
+	defaultImage: {
+		data: Buffer,
+		contentType: String,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	type: { // All, Concerts, Exhibitions, Sport
+		type: String,
+		required: true,
+	},
+	location: {
+		type: String,
+		required: true,
+	},
+	date: {
+    type: Date,
+    required: true
+  },
+	price: {
+		type: Number,
+		required: true,
+	},
+});
 const Event = mongoose.model('Event', eventSchema);
 
 // TODO: make Ticket schema and object (active & archived)
