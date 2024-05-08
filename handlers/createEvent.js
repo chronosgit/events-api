2a948029467273600f44feade2a246ad13c939b6
chronosgit <warfacemeister@mail.ru>
2024-05-09 00:15:47 +0500

    feat: updated createEvent handler to support images

--- handlers/createEvent.js
+++ handlers/createEvent.js
@@ -1,8 +1,10 @@
+const fs = require("fs");
+const path = require("path");
 const Models = require("../models");
 
 const createEvent = async (req, res) => {
 	try {
-		const {name, type, location, price, date} = req.body;
+		const {name, type, location, price, date, image} = req.body;
 		const validEventTypes = [
 			"concerts", "sport",
 			"exhibitions", "all", 
@@ -20,15 +22,29 @@ const createEvent = async (req, res) => {
 			return;
 		}
 
-		// TODO: handle images
-
+		const rootDir = path.resolve(__dirname, '../');
 		const newEvent = new Models.Event({
 			name: name, type: type, price: price, 
 			location: location, date: new Date(date),
+			defaultImage: {
+				data: fs.readFileSync(path.join(rootDir + "/images/defaultEventPic.jpg")),
+				type: 'Buffer',
+			},
 		});
 		await newEvent.save();
 
-		res.status(201).send(newEvent);
+		const profilePicBuffer = newEvent.hasImage ? newEvent.image : newEvent.defaultImage; 
+		const profilePicBase64 = Buffer.from(profilePicBuffer.data, "base64").toString("base64");
+
+		const resBody = {
+			name: newEvent.name,
+			type: newEvent.type,
+			price: newEvent.price,
+			location: newEvent.location,
+			date: newEvent.date,
+			image: profilePicBase64,
+		};
+		res.status(201).send(resBody);
 	} catch(error) {
 		console.error(error);
 
