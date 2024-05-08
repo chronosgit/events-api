7d063087620acdce8a99fd3865ebba408743bf86
chronosgit <warfacemeister@mail.ru>
2024-05-08 20:58:41 +0500

    feat: added todo comments

--- app.js
+++ app.js
@@ -4,23 +4,33 @@ const cors = require('cors');
 const fileUpload = require("express-fileupload");
 require('dotenv').config();
 
-// Initializing app
+// NOTE: Initializing app
+
 const app = express();
 
-// Middlewares
+// NOTE: Middlewares
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-// Connecting to MongoDB
+// NOTE: Connecting to MongoDB
+
 mongoose.connect("mongodb://127.0.0.1:27017/swift")
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Failed to connect to MongoDB', err));
 
-// Starting server
+// NOTE: Starting server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Routes
+// NOTE: Routes
+
+// TODO: route for getting events
+
+// TODO: route for posting application
+
+// TODO: route for getting notifications of events
