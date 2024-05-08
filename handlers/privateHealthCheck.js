a616e0db368d763ce5b348f4d27b604e68656f6a
chronosgit <warfacemeister@mail.ru>
2024-05-08 22:52:04 +0500

    feat: added privateHealthCheck handler

---
+++ handlers/privateHealthCheck.js
@@ -0,0 +1,16 @@
+const privateHealthCheck = async (req, res) => {
+	try {
+		const user = req.user;
+
+		// WARN: remove later
+		console.log(user);
+
+		res.status(200).send({message: "Events API JWT middleware is working correctly"})
+	} catch(error) {
+		console.error(error);
+
+		res.status(500).send({error: "Private health check resulted in error"});
+	}
+};
+
+module.exports = privateHealthCheck;
