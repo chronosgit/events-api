59a69b5ae41511afc88dc2b870bcd89d01cab0c0
chronosgit <warfacemeister@mail.ru>
2024-05-09 01:17:21 +0500

    feat: updated createTicket.js logic to support new model with schema

--- handlers/createTicket.js
+++ handlers/createTicket.js
@@ -2,27 +2,40 @@ const Models = require("../models");
 
 const createTicket = async (req, res) => {
 	try {
+		const {quantity, price} = req.body;
+
+		if(quantity == null || price == null) {
+			res.status(400).send({error: "Request must contain quantity and price"});
+			return;
+		}
+
 		const username = req.user.username;
 		const user = await Models.User.findOne({username: username});
 		const eventId = req.query.eventId;
 		
-
 		if(!user) {
 			res.status(404).send({error: "User, creating a ticket, doesn't exist"});
+			return;
 		}
 
-		const userTicketsIds = user.tickets;
-		for(let userTicketId of userTicketsIds) {
-			if(String(userTicketId._id) === eventId) {
-				res.status(409).send({error: "Ticket on this event already exists"});
+		// TODO: check if ticket already exists
+		const tickets = await Models.Ticket.find({user: user._id});
+		for(let ticket of tickets) {
+			const ticketEventId = String(ticket.event);
+
+			if(ticketEventId === eventId) {
+				res.status(409).send({error: "Ticket on this event by user already exists"})
 				return;
 			}
 		}
 
 		const event = await Models.Event.findOne({_id: eventId});
-		await Models.User.findOneAndUpdate({username: username}, {$push: {tickets: event}});
+		const newTicket = new Models.Ticket({
+			quantity, price, event: event, user: user
+		});
+		await newTicket.save();
 
-		res.status(200).send({message: "Ticket is created"});
+		res.status(201).send(newTicket);
 	} catch(error) {
 		console.error(error);
 
